dda32a523cbc583ed863f9e3a8d97a2092e0d322
Ole Lehmann <0xwlgrinder@gmail.com>
2026-07-02 12:37:36 +0300

    Update index.js

--- digitalism_web/backend_node/index.js
+++ digitalism_web/backend_node/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors');
+const cors = require('cors'); 
 // const db = require('./db'); // No longer needed
 const { identifyUser } = require('./middleware/auth');
 
